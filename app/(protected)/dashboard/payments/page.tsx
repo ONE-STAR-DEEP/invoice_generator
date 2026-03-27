572c852efe6e7908e2ae98f31a82c028fd407715
Deependra Chauhan <chauhandeependra2001@gmail.com>
2026-03-27 17:29:14 +0530

    Update

--- app/(protected)/dashboard/payments/page.tsx
+++ app/(protected)/dashboard/payments/page.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const PaymentsPage = () => {
   return (
-    <div>PaymentsPage</div>
+    <div className="flex flex-col flex-1 space-y-6 min-h-0">
+
+      <section className="flex items-center justify-between border px-8 py-4 rounded-2xl bg-muted/50">
+        <h1 className="font-semibold text-lg">Payments</h1>
+
+      </section>
+
+      <section className="flex-1 bg-muted/50 border border-border p-4 rounded-2xl overflow-auto min-h-0">
+        <div className='h-full w-full flex items-center justify-center'>
+          <p className='text-5xl font-bold'>Cooming Soon</p>
+        </div>
+
+      </section>
+
+    </div>
   )
 }
 
